19f4c0fd78f665850343fe1152c80962ba4fab18
Nate <mac17039@byui.edu>
2019-02-25 16:57:02 -0700

    revert back to a previously working version
    This reverts commit 2847338a2f80b6a7d5aeba78dd50df8ce897ac72.

--- app/src/main/java/example/com/hometherapy/MainActivity.java
+++ app/src/main/java/example/com/hometherapy/MainActivity.java
@@ -12,10 +12,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // This is a comment by Nate
-    // This is a comment by Emile (fingers crossed)
-    // Nate's next comment
-    // Emile comment
+    //This is a comment by Nate
+    //This is a comment by Emile (fingers crossed)
+    //Nate's next comment
+    //Emile comment
     // Eric new comment
 
     // create a new message
