9dedaaa5b9a68c520c37b10305ec6d94d5a65aa7
Eric Burns <eburns000@byui.edu>
2019-02-25 21:27:15 -0800

    update MainActivity add test button by Eric

--- app/src/main/java/example/com/hometherapy/MainActivity.java
+++ app/src/main/java/example/com/hometherapy/MainActivity.java
@@ -1,15 +1,34 @@
 package example.com.hometherapy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button _btnGoToSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        _btnGoToSignIn = (Button) findViewById(R.id.btnGoToSignIn);
+
+        _btnGoToSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // create intent to move to sign in screen
+                Intent intentSignInScreen = new Intent (MainActivity.this, signIn.class);
+
+                // start the new activity
+                startActivity(intentSignInScreen);
+            }
+        });
     }
 
     // create a new message
