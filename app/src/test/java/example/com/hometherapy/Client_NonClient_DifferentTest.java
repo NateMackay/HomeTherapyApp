fb6138cdea015e17cf30498500b74222cbdd2a7b
Emile Moosman <emilemoosman@byui.edu>
2019-03-06 05:25:34 +1100

    Added a test class, to test that a ClientUser, and NonClientUser are different.

---
+++ app/src/test/java/example/com/hometherapy/Client_NonClient_DifferentTest.java
@@ -0,0 +1,19 @@
+package example.com.hometherapy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Client_NonClient_DifferentTest {
+
+    private User userOne = new ClientUser("sam@gmail.com", "qwerty123", "Sam",
+            "Little", 98653241, 1234, "MatthewJordan");
+
+    private User userTwo = new NonClientUser("sam@gmail.com","qwerty456", "Sam",
+            "Little", 65874125, 5678, "Admin");
+
+    @Test
+    public void Clients_And_NonClients_AreDifferentPeople() {
+        assertNotSame("Should not be the same Users.", userOne, userTwo);
+    }
+}
