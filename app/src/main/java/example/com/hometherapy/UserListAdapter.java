2da66e7ebaf131d52a14a4dd4a046c6052711e9c
Eric Burns <eburns000@byui.edu>
2019-03-09 11:31:02 -0800

    Add NonNull.java (annotation) add Nullable.java (annotation) Add UserListAdapter.java - custom array adapter class for Users.java Add listview_row_design_user_1.xml - custom listview for custom UserListAdapter.java Update activity_users.xml

---
+++ app/src/main/java/example/com/hometherapy/UserListAdapter.java
@@ -0,0 +1,49 @@
+package example.com.hometherapy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class UserListAdapter extends ArrayAdapter<User> {
+
+    private Context _context;
+    private List<User> _userList;
+
+    public UserListAdapter(@NonNull Context context, @NonNull List<User> userList) {
+        super(context, 0, userList);
+        _context = context;
+        _userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItem = convertView;
+        if(listItem == null) {
+            listItem = LayoutInflater.from(_context).inflate(R.layout.listview_row_design_user_1, parent, false);
+        }
+
+        User currentUser = _userList.get(position);
+
+        TextView name = (TextView) listItem.findViewById(R.id.tvRowUserName);
+        name.setText(String.format("%s, %s", currentUser.getLastName(), currentUser.getFirstName()));
+
+        TextView email = (TextView) listItem.findViewById(R.id.tvRowEmail);
+        email.setText(currentUser.getEmail());
+
+        TextView accountType = (TextView) listItem.findViewById(R.id.tvRowAccountType);
+        accountType.setText(String.format("Acct Type: %s", currentUser.get_accountType()));
+
+        TextView status = (TextView) listItem.findViewById(R.id.tvRowStatus);
+        status.setText(String.format("Status: %s", currentUser.get_status()));
+
+        return listItem;
+    }
+}
+
