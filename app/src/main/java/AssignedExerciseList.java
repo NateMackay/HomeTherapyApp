7da9461e71a7d3ddf2c070377d421371509db9f4
Eric Burns <eburns000@byui.edu>
2019-03-09 15:37:40 -0800

    Update AddExerciseToClient.java, ExerciseList.java, Exercises.java

--- app/src/main/java/AssignedExerciseList.java
+++ app/src/main/java/AssignedExerciseList.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import example.com.hometherapy.Exercise;
+
 public class AssignedExerciseList {
+
+    private List<AssignedExercise> _assignedExerciseList;
+
+    public AssignedExerciseList() { _assignedExerciseList = new ArrayList<>(); }
+
+    public void addAssignedExercise(AssignedExercise assignedExercise) {
+        _assignedExerciseList.add(assignedExercise);
+    }
+
+    // getter
+    public List<AssignedExercise> getAssignedExerciseList() {
+        return _assignedExerciseList;
+    }
+
+    // output current list of assigned exercises
+    @Override
+    public String toString() {
+        return "AssignedExerciseList{" +
+                "_assignedExerciseList=" + _assignedExerciseList +
+                '}';
+    }
 }
