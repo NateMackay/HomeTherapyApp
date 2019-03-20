8a0fd7b8f47fc215232ddbea97d2e77a3217f5bd
Emile Moosman <emilemoosman@byui.edu>
2019-03-21 03:43:37 +1100

    Make read only, add a complete button with functionality.

--- app/src/main/java/example/com/hometherapy/MyExercise.java
+++ app/src/main/java/example/com/hometherapy/MyExercise.java
@@ -2,26 +2,17 @@ package example.com.hometherapy;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 /**
  * Client's view of a given assigned exercise. Clients navigate to this
@@ -40,6 +31,7 @@ public class MyExercise extends AppCompatActivity {
     // name shared preferences
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String ASSIGNED_EXERCISE_DATA = "assignedExerciseData";
+    public static final String USER_DATA = "userData";
 
     // Key for extra message for user email address to pass to activity
     public static final String MSG_USER_EMAIL = "example.com.hometherapy.USEREMAIL";
@@ -53,6 +45,11 @@ public class MyExercise extends AppCompatActivity {
     public static final String MSG_DISCIPLINE = "example.com.hometherapy.DISCIPLINE";
     public static final String MSG_ASSIGNMENT = "example.com.hometherapy.ASSIGNMENT";
     public static final String MSG_VIDEO_LINK = "example.com.hometherapy.VIDEO_LINK";
+    public static final String MSG_MY_POINTS = "example.com.hometherapy.MY_POINTS";
+
+    //
+    private static final String COMPLETED = "complete";
+
 
     // member variables
     private Gson _gson;
@@ -60,13 +57,17 @@ public class MyExercise extends AppCompatActivity {
     private AssignedExerciseList _currentAssignedExercises;
     private List<AssignedExercise> _tempAssignedExerciseList;
     private AssignedExercise _currentAssignedExercise;
+    private UserList _currentUsers;
+    private List<User> _tempUserList;
+    private User _currentUser;
     private String _assignment;
     private String _exercise;
     private String _discipline;
     private String _modality;
     private String _linkToVideo;
-    private int _pointValue;
+    private Integer _pointValue;
     private String _status;
+    private Integer _myPoints;
 
     private boolean _isIntentAdd;
     private Integer _assignedExerciseID;
@@ -78,13 +79,15 @@ public class MyExercise extends AppCompatActivity {
     private TextView _tvMyModality;
     private TextView _tvLinkToVideo;
     private Button _btnMyComplete;
+    private Button _btnMyExercises;
     private TextView _myEPointValue;
-    private Spinner _spinMyEStatus;
+    private TextView _tvMyEStatus;
+    private TextView _tvWellDoneMSG;
 
     // array adapter for spinner views
-    private String _exerciseStatusNames[] = {"Status", "not started", "started", "complete", "on hold", "inactive"};
-    private String _pointValueOptions[] = {"5", "10", "15", "20", "25"};
-    private ArrayAdapter<String> _adapterStatus;
+    //private String _exerciseStatusNames[] = {"Status", "not started", "started", "complete", "on hold", "inactive"};
+    //private String _pointValueOptions[] = {"5", "10", "15", "20", "25"};
+    //private ArrayAdapter<String> _adapterStatus;
     //private ArrayAdapter<String> _adapterPointValue;
 
     @Override
@@ -98,21 +101,24 @@ public class MyExercise extends AppCompatActivity {
         _tvMyDiscipline = (TextView) findViewById(R.id.tvMyDiscipline);
         _tvMyModality = (TextView) findViewById(R.id.tvMyModality);
         _tvLinkToVideo = (TextView) findViewById(R.id.tvLinkToVideo);
-        _btnMyComplete = (Button) findViewById(R.id.btnMyComplete);
-        _myEPointValue = (TextView) findViewById(R.id.myEPointValue);
-        _spinMyEStatus = (Spinner) findViewById(R.id.spinMyEStatus);
+        _btnMyComplete = (Button) findViewById(R.id.btnComplete);
+        _btnMyExercises = (Button) findViewById(R.id.btnGoToExercises);
+        _myEPointValue = (TextView) findViewById(R.id.tvMyEPointValue);
+        _tvMyEStatus = (TextView) findViewById(R.id.tvMyEStatus);
+        _tvWellDoneMSG = (TextView) findViewById(R.id.tvWellDoneMsg);
 
         // adapters
         //_adapterPointValue = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, _pointValueOptions);
-        _adapterStatus = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, _exerciseStatusNames);
+        //_adapterStatus = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, _exerciseStatusNames);
 
         // set adapters
         //_myEPointValue.setAdapter(_adapterPointValue);
-        _spinMyEStatus.setAdapter(_adapterStatus);
+        //_spinMyEStatus.setAdapter(_adapterStatus);
 
-        // open up database for given assigned exercise (shared preferences)
+        // open up database for given assigned exercise, and current user. (shared preferences)
         _sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         String jsonAssignedExerciseList = _sharedPreferences.getString(ASSIGNED_EXERCISE_DATA, "");
+        String jsonUserList = _sharedPreferences.getString(USER_DATA, "");
 
         // initialize GSON object
         _gson = new Gson();
@@ -134,6 +140,30 @@ public class MyExercise extends AppCompatActivity {
         _currentUserEmail = thisIntent.getStringExtra(MSG_USER_EMAIL);
 
         Log.d(TAG, "verify current user: " + _currentUserEmail);
+
+        // Get User account to update user rewards, by,
+        // deserializing sharedPrefs JSON user database into List of Users
+        _currentUsers = _gson.fromJson(jsonUserList, UserList.class);
+
+        if (_currentUsers != null) {
+            List<User> tempUserList = _currentUsers.getUserList();
+
+            // find matching user account by email
+            // reference: section 3.5 on this page https://www.baeldung.com/find-list-element-java
+            _currentUser = tempUserList.stream()
+                    .filter(user -> _currentUserEmail.equals(user.getEmail()))
+                    .findAny()
+                    .orElse(null);
+
+            // Pull myPoints from current User account
+            _myPoints = _currentUser.get_myPoints();
+            if (_myPoints == null) {
+                _myPoints = 0;
+            }
+
+            Log.d(TAG, "User: " + _currentUser.getFirstName()
+                    + " has current rewards total of " + _currentUser.get_myPoints() + ".");
+        }
 
         /**
         // depending on whether intent is to either add or edit a client exercise
@@ -185,13 +215,21 @@ public class MyExercise extends AppCompatActivity {
                 _modality = _currentAssignedExercise.get_modality();
                 _linkToVideo = _currentAssignedExercise.get_videoLink();
                 _pointValue = _currentAssignedExercise.get_pointValue();
-                //_status = _currentAssignedExercise.get_status();
+                _status = _currentAssignedExercise.get_status();
+
+                //
+                if (_status.equals(COMPLETED)) {
+                    hasCompleted();
+                }
             }
+
+        Log.d(TAG, "User: " + _currentUser.getFirstName()
+                + " point value of current exercise is " + _pointValue + ".");
 
             // set status and point value spinners based on what is in assigned exercise
             // first, get indices of these values
             //int iSpinAETCPointValue = Arrays.asList(_pointValueOptions).indexOf(_currentAssignedExercise.get_pointValue().toString());
-            int iSpinAETCStatus = Arrays.asList(_exerciseStatusNames).indexOf(_currentAssignedExercise.get_status());
+            //int iSpinAETCStatus = Arrays.asList(_exerciseStatusNames).indexOf(_currentAssignedExercise.get_status());
 
             // set spinner values based on current assigned exercise
             // note that a value of -1 means indexOf() did not find value searching for
@@ -199,9 +237,9 @@ public class MyExercise extends AppCompatActivity {
             //    _myEPointValue.setSelection(iSpinAETCPointValue);
             //}
 
-            if (iSpinAETCStatus >= 0) {
-                _spinMyEStatus.setSelection(iSpinAETCStatus);
-            }
+            //if (iSpinAETCStatus >= 0) {
+            //    _spinMyEStatus.setSelection(iSpinAETCStatus);
+            //}
         //}
 
         // set view text values
@@ -211,8 +249,9 @@ public class MyExercise extends AppCompatActivity {
         _tvMyModality.setText(_modality);
         _tvLinkToVideo.setText(_linkToVideo);
         _myEPointValue.setText(Integer.toString(_pointValue));
+        _tvMyEStatus.setText(_status);
 
-        //
+        /**
         _spinMyEStatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
             {
@@ -226,13 +265,13 @@ public class MyExercise extends AppCompatActivity {
             {
 
             }
-        });
+        });**/
 
         // we could alternatively set the index value for the spinners in the above if/else statement
         // and then set the spinners here per the index, but I believe the default value is 0
         // which for a new exercise, would be set anyway by the user.
 
-        // listener for save changes button
+        // listener for Completed Exercises button
         _btnMyComplete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -254,29 +293,77 @@ public class MyExercise extends AppCompatActivity {
                 //    _currentAssignedExercise.set_discipline(_tvMyDiscipline.getText().toString());
                 //    _currentAssignedExercise.set_modality(_tvMyModality.getText().toString());
                 //   _currentAssignedExercise.set_videoLink(_tvLinkToVideo.getText().toString());
-                    _currentAssignedExercise.set_status(_spinMyEStatus.getSelectedItem().toString());
+                //    _currentAssignedExercise.set_status(_spinMyEStatus.getSelectedItem().toString());
                 //    _currentAssignedExercise.set_pointValue(Integer.parseInt(_myEPointValue.getSelectedItem().toString()));
                 //}
 
+
+
+                // Complete button was clicked so update rewards
+                _currentUser.set_myPoints(_myPoints + _pointValue);
+                Log.d(TAG, "User: " + _currentUser.getFirstName()
+                        + " Rewards after completion is " + _currentUser.get_myPoints() + ".");
+
+                // Update user assigned exercise status
+                _currentAssignedExercise.set_status(COMPLETED);
+                Log.d(TAG, "User: " + _currentUser.getFirstName()
+                        + " Exercise status is " + _currentAssignedExercise.get_status() + ".");
+
                 // convert updated assigned exercise list back to JSON format
-                String updatedList = _gson.toJson(_currentAssignedExercises);
+                String updatedAssignedExerciseList = _gson.toJson(_currentAssignedExercises);
+
+                // convert updated UserList object back to JSON format
+                String updatedUserList = _gson.toJson(_currentUsers);
+
+                _currentUser.set_status(COMPLETED);
+                Log.d(TAG, "2nd time, User: " + _currentUser.getFirstName()
+                        + " Exercise status is " + _currentUser.get_status() + ".");
 
                 // update Shared Prefs
                 SharedPreferences.Editor editor = _sharedPreferences.edit();
-                editor.putString(ASSIGNED_EXERCISE_DATA, updatedList);
+                editor.putString(ASSIGNED_EXERCISE_DATA, updatedAssignedExerciseList);
+                editor.putString(USER_DATA, updatedUserList);
                 editor.apply();
 
                 // display contents for testing purposes
-                String fromSharedPrefs = _sharedPreferences.getString(ASSIGNED_EXERCISE_DATA, "");
-                Log.d(TAG, "fromSharedPrefs: " + fromSharedPrefs);
+                String fromAEDSharedPrefs = _sharedPreferences.getString(ASSIGNED_EXERCISE_DATA, "");
+                Log.d(TAG, "fromSharedPrefs (Assigned Exercises): " + fromAEDSharedPrefs);
+                // display contents for testing purposes
+                String fromUSharedPrefs = _sharedPreferences.getString(USER_DATA, "");
+                Log.d(TAG, "fromSharedPrefs (Users): " + fromUSharedPrefs);
 
-                Intent intentClientExercises = new Intent(MyExercise.this, MyExercises.class);
-                intentClientExercises.putExtra(MSG_USER_EMAIL, _currentUserEmail);
-                startActivity(intentClientExercises);
+                //Intent intentUpdate = new Intent();
+                //intentUpdate.putExtra(MSG_USER_EMAIL, _currentUserEmail);
+                //startActivity(intentUpdate);
 
+                //
+                hasCompleted();
             }
         });
 
+        // listener for Completed Exercises button
+        _btnMyExercises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentClientExercises = new Intent(MyExercise.this, MyExercises.class);
+                intentClientExercises.putExtra(MSG_USER_EMAIL, _currentUserEmail);
+                startActivity(intentClientExercises);
+            }
+        });
+
+    }
+
+    /**
+     * If assigned exercise is already completed, then make the below changes to the view when called.
+     */
+    public void hasCompleted() {
+        _btnMyComplete.setText("Completed");
+        _btnMyComplete.setBackgroundColor(Color.parseColor("#BA8BC34A"));
+        _tvWellDoneMSG.setVisibility(View.VISIBLE);
+        _btnMyComplete.setEnabled(false);
+
+        // Trying to work out how to change the button to a circle??
+        // _btnMyComplete.
     }
 
 }
