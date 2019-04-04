70c94dda1bbd5766768a3544c781f26ca2cd70c1
Eric Burns <eburns000@byui.edu>
2019-04-04 10:30:12 -0700

    Update Users  - modified layout so listview takes up entire screen automatically and scrolls properly  - cleaned up users.java

--- app/src/main/java/example/com/hometherapy/Users.java
+++ app/src/main/java/example/com/hometherapy/Users.java
@@ -1,7 +1,6 @@
 package example.com.hometherapy;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -26,7 +25,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
