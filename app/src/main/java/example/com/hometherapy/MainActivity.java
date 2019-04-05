578669b983f3106e410394e40f16c84c8f562c78
Eric Burns <eburns000@byui.edu>
2019-04-04 18:48:35 -0700

    Add splash screen  - main activity is now the splash screen and delays moving to sign-in    screen until after 5000ms

--- app/src/main/java/example/com/hometherapy/MainActivity.java
+++ app/src/main/java/example/com/hometherapy/MainActivity.java
@@ -1,16 +1,13 @@
 package example.com.hometherapy;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 /**
  * Splash screen.
- * Temporary landing page for testing app.
  * @author Team06
  * @version 1.0
  * @since 2019-03-19
@@ -20,163 +17,26 @@ public class MainActivity extends AppCompatActivity {
     // for log
     private static final String TAG = "Main_Activity";
 
-    private Button _btnGoToSignIn;
-    private Button _btnGoToExercises;
-    private Button _btnGoToMyClients;
-    private Button _btnTestNavigation;
-    private Button _btnGoToMyMessages;
-    private Button _btnGoToUsers;
-    private Button _btnGoToMyExercises;
-    private Button _btnClearSharedPrefs;
-
-    private String _userData;
-    private String _exerciseData;
-    private String _assignedExerciseData;
-
-    // name Shared Preferences
-    public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String ASSIGNED_EXERCISE_DATA = "assignedExerciseData";
-    public static final String USER_DATA = "userData";
-    public static final String EXERCISE_DATA = "exerciseData";
-    public static final String LOGIN_USER = "loginUser";
-
-    // private member variables
-    private SharedPreferences _sharedPreferences;
+    // duration of delay for splash screen
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        _btnGoToSignIn =     (Button) findViewById(R.id.btnGoToSignIn);
-        _btnGoToExercises =  (Button) findViewById(R.id.btnGoToExercises);
-        _btnGoToMyClients =  (Button) findViewById(R.id.btnGoToMyClients);
-        _btnGoToMyMessages = (Button) findViewById(R.id.btnGoToMyMessages);
-        _btnTestNavigation = (Button) findViewById(R.id.btnTestNavigation);
-        _btnGoToUsers      = (Button) findViewById(R.id.btnGoToUsers);
-        _btnGoToMyExercises = (Button) findViewById(R.id.btnGoToMyExercises);
-        _btnClearSharedPrefs = (Button) findViewById(R.id._btnClearSharedPrefs);
-
-        // open up Shared Preferences db
-        _sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        _userData = _sharedPreferences.getString(USER_DATA, "");
-        _exerciseData = _sharedPreferences.getString(EXERCISE_DATA, "");
-        _assignedExerciseData = _sharedPreferences.getString(ASSIGNED_EXERCISE_DATA, "");
-
-        // log shared preference data
-        Log.d(TAG, "user data: " + _userData);
-        Log.d(TAG, "exercise data: " + _exerciseData);
-        Log.d(TAG, "assigned exercise data: " + _assignedExerciseData);
-
-        // Sign in screen
-        _btnGoToSignIn.setOnClickListener(new View.OnClickListener() {
+        // set up delay and then move on to sign in screen
+        new Handler().postDelayed(new Runnable(){
             @Override
-            public void onClick(View v) {
+            public void run() {
+                // move to Sign-in after
+                Intent mainIntent = new Intent(MainActivity.this, SignIn.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
 
-                // create intent to move to sign in screen
-                Intent intentSignInScreen = new Intent(MainActivity.this, SignIn.class);
-
-                // start the new activity
-                startActivity(intentSignInScreen);
+                Log.d(TAG, "intent to signIn.class");
             }
-        });
+        }, SPLASH_DISPLAY_LENGTH);
 
-        // List of Exercises screen
-        _btnGoToExercises.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // create intent to move to sign in screen
-                Intent intent_Exercises_Screen = new Intent(MainActivity.this, Exercises.class);
-
-                // start the new activity
-                startActivity(intent_Exercises_Screen);
-            }
-        });
-
-        // Exercises screen
-        _btnGoToMyExercises.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // create intent to move to sign in screen
-                Intent intent_My_Exercises_Screen = new Intent(MainActivity.this, MyExercises.class);
-
-                // start the new activity
-                startActivity(intent_My_Exercises_Screen);
-            }
-        });
-
-        // My Clients list screen
-        _btnGoToMyClients.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // create intent to move to sign in screen
-                Intent intentSignInScreen = new Intent(MainActivity.this, MyClients.class);
-
-                // start the new activity
-                startActivity(intentSignInScreen);
-            }
-        });
-
-        // Test Navigation screen
-        _btnTestNavigation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // create intent to move to sign in screen
-                Intent intentTestNavScreen = new Intent(MainActivity.this, TestNavigation.class);
-
-                // start the new activity
-                startActivity(intentTestNavScreen);
-            }
-        });
-
-        // My Messages screen
-        _btnGoToMyMessages.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // create intent to move to sign in screen
-                Intent intentMessagesScreen = new Intent(MainActivity.this, MyMessages.class);
-
-                // start the new activity
-                startActivity(intentMessagesScreen);
-            }
-        });
-
-        // Users screen
-        _btnGoToUsers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // create intent to move to sign in screen
-                Intent intentSignInScreen = new Intent(MainActivity.this, Users.class);
-
-                // start the new activity
-                startActivity(intentSignInScreen);
-            }
-        });
-
-        // cleared shared preferences
-        _btnClearSharedPrefs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               SharedPreferences.Editor editor = _sharedPreferences.edit();
-               editor.putString(USER_DATA, "");
-               editor.putString(EXERCISE_DATA, "");
-               editor.putString(ASSIGNED_EXERCISE_DATA, "");
-               editor.putString(LOGIN_USER, "");
-               editor.apply();
-            }
-
-        });
-
-    }
-
-    // create a new message
-    public void createNewMessage(View view) {
-        Messaging myMessage = new Messaging();
     }
 }
